0c655a1a33a197a35eb113f3cd6369a14a0b64d3
ccwanggl <ccwanggl@163.com>
2018-11-01 09:01:51 +0800

    update

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,7 +1,61 @@
 #include <windows.h>
 
+
+#define internal static 
+#define local_persist static 
+#define global_variable static 
+
+//TODO(guoliang): This is a global for now
+global_variable bool Running;
+
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+
+internal void
+win32ResizeDIBSection(int Width, int Height)
+{
+	if (BitmapHandle)
+	{
+		DeleteObject(BitmapHandle);
+	}
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biSizeImage = 0;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+	BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+	BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+	BitmapInfo.bmiHeader.biClrUsed = 0;
+	BitmapInfo.bmiHeader.biClrImportant = 0;
+
+	HDC DeviceContext = GetCompatibleDC(0);
+
+	BitmapHandle = CreateDIBSection(DeviceContext,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		&BitmapMemory,
+		0, 0);
+	ReleaseDC(0, DeviceContext);
+}
+
+internal void
+win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+	StretchDIBits(DeviceContext,
+		X, Y, Width, Height,
+		X, Y, Width, Height,
+
+		DIB_RGB_COLORS,
+		SRCCOPY);
+}
+
 LRESULT CALLBACK 
-WindowProc(HWND Window,
+win32WindowProc(HWND Window,
            UINT Message,
            WPARAM wParam,
            LPARAM lParam)
@@ -11,20 +65,25 @@ WindowProc(HWND Window,
     {
         case WM_SIZE:
         {
-            OutputDebugString("WM_SIZE\n");
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+			int Width = ClientRect.right - ClientRect.left;
+			int Height = ClientRect.bottom - ClientRect.top;
+			win32ResizeDIBSection(Width, Height);
         }break;
         case WM_DESTROY:
         {
-            OutputDebugString("WM_DESTORY\n");
+			Running = false;
+			//TODO: Handle this as an error - recreate window?
         }break;
         case WM_CLOSE:
         {
-			PostQuitMessage(0);
-            OutputDebugString("WM_CLOSE\n");
+			//TODO: Handle this with a message to the user?
+			Running = false;
         }break;
         case WM_ACTIVATEAPP:
         {
-            OutputDebugString("WM_ACTIVATEAPP\n");
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
         }break;
         case WM_PAINT:
         {
@@ -34,6 +93,7 @@ WindowProc(HWND Window,
             int Y = Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			win32UpdateWindow(DeviceContext, X, Y, Width, Height);
             PatBlt(DeviceContext, X, Y, Width, Height,  WHITENESS);
 
             EndPaint(Window, &Paint);
@@ -54,7 +114,7 @@ WinMain(HINSTANCE hInstance,
     WNDCLASS WindowClass = {};
 
     WindowClass.style           = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc     = WindowProc;
+    WindowClass.lpfnWndProc     = win32WindowProc;
     WindowClass.hInstance       = hInstance; 
     WindowClass.lpszClassName   = "Handmade";
 
@@ -76,14 +136,15 @@ WinMain(HINSTANCE hInstance,
                   0);
         if(WindowHandle)
         {
-            for(;;)
+			Running = true;
+            while(Running)
             {
                 MSG Message;
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
                 if(MessageResult > 0)
                 {
                     TranslateMessage(&Message);
-                    DispatchMessage(&Message);
+                    DispatchMessageA(&Message);
                 }
                 else
                 {
