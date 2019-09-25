6ce37db8030fb99a406b5a009b100b03add3be97
guoliang <ccwanggl@163.com>
2019-09-25 16:01:48 +0800

    Add some comments to misc\shell.bat

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,3 +1,10 @@
 @echo off
+
+REM
+REM  To  run this at startup, use this as your shortcut target:
+REM  %windir%\sytem32\cmd.exe /k w:\handmade\misc\shell.bat
+REM
+
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set path=w:\handmade\misc;%path%
